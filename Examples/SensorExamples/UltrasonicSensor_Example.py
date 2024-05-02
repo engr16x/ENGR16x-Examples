5c87624bde536e8b5dcb4593d3d97a1717c49534
unknown <ngrzegor@purdue.edu>
2024-05-02 00:18:50 -0400

    McConkeyDonkeyWonkyMonky

--- Examples/SensorExamples/UltrasonicSensor_Example.py
+++ Examples/SensorExamples/UltrasonicSensor_Example.py
@@ -1,1 +1,42 @@
-# Noah
+# UltrasonicSensor_Example.py
+
+# Created by Noah Grzegorek on behalf of the ENGR 16X Teaching Team
+
+# More info and instruction on using this sensor can be found in the Modules folder
+# on your Pi's Desktop 
+
+# import the necessary packages for your sensor and code
+from basehat import UltrasonicSensor
+import time
+
+def main():
+
+    # set the pin to be used
+    # if sensor is plugged into port D5, pin1 should be 5
+    # make sure to only plug in Ultrasonic Sensor to digital ports of the Grove BaseHAT (D5, D16, D18, D22, D24, D26)
+    pin = 5
+
+    # Initializing the sensor so the function within the class can be used
+    ultra = UltrasonicSensor(pin)
+
+    try: 
+        while True:
+            try:
+                # Getting the current distance value from the ultrasonic sensor
+                value = ultra.getDist
+
+                # Printing the current reading and waiting to take another reading for 1 second
+                print('{} cm'.format(value))
+                time.sleep(1)
+
+            except IOError:
+                print ("\nError occurred while attempting to read values.")
+
+
+    except KeyboardInterrupt:
+        print("\nCtrl+C detected. Exiting...")
+        break
+        
+
+if __name__ == '__main__':
+    main()
--- basehat/UltrasonicSensor.py
+++ basehat/UltrasonicSensor.py
@@ -1,10 +1,96 @@
-# for UltrasonicSensor, follow this code:
-# https://github.com/Seeed-Studio/grove.py/blob/master/grove/grove_ultrasonic_ranger.py
+# UltrasonicSensor.py
 
-# For most of these, you will simply just need to rename the class to make the name simpler.
-# You may change property namees/functions to be more consistent or easier to remember, but 
-# that is not 100% necessary all the time.
-# If you are confused on how the class works, consider changing it to be more intuitive
-# so it is also easier for students
+# Created by Noah Grzegorek on behalf of the ENGR 16X Teaching Team
 
-# Noah
+### DO NOT MODIFY CODE IN THIS FILE ###
+
+# This is the code for Grove - Ultrasonic Ranger
+# (https://www.seeedstudio.com/Grove-Ultrasonic-Ranger-p-960.html)
+# which is a non-contact distance measurement module which works with 40KHz sound wave. 
+#
+# This is the library for Grove Base Hat which used to connect grove sensors for raspberry pi.
+#
+
+'''
+## License
+
+The MIT License (MIT)
+
+Grove Base Hat for the Raspberry Pi, used to connect grove sensors.
+Copyright (C) 2018  Seeed Technology Co.,Ltd. 
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+'''
+
+import sys
+import time
+from grove.gpio import GPIO
+
+# This usleep is to have used to have the sensor sleep for a certain amount of microseconds
+usleep = lambda x: time.sleep(x / 1000000.0)
+
+# These global variables are used as part of the distance calculations the sensor performs
+_TIMEOUT1 = 1000
+_TIMEOUT2 = 10000
+
+class UltrasonicSensor(object):
+    def __init__(self, pin):
+        self.dio = GPIO(pin)
+
+    # This function calculates the distance that the nearest object it can detect is from the sensor in centimeters
+    @property
+    def getDist(self):
+        self.dio.dir(GPIO.OUT)
+        self.dio.write(0)
+        usleep(2)
+        self.dio.write(1)
+        usleep(10)
+        self.dio.write(0)
+
+        self.dio.dir(GPIO.IN)
+
+        t0 = time.time()
+        count = 0
+        while count < _TIMEOUT1:
+            if self.dio.read():
+                break
+            count += 1
+        if count >= _TIMEOUT1:
+            return None
+
+        t1 = time.time()
+        count = 0
+        while count < _TIMEOUT2:
+            if not self.dio.read():
+                break
+            count += 1
+        if count >= _TIMEOUT2:
+            return None
+
+        t2 = time.time()
+
+        dt = int((t1 - t0) * 1000000)
+        if dt > 530:
+            return None
+
+        distance = ((t2 - t1) * 1000000 / 29 / 2)    # cm
+
+        return distance
+
+
