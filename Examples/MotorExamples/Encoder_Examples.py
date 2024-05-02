fef90cb19a2f31e981c88fcab503d0d2f2307a29
unknown <ngrzegor@purdue.edu>
2024-05-02 01:42:05 -0400

    bang

--- Examples/MotorExamples/Encoder_Examples.py
+++ Examples/MotorExamples/Encoder_Examples.py
@@ -1,7 +1,3 @@
-# print out speed, apostion, and position in a 'while True:' loop
-# https://buildhat.readthedocs.io/en/latest/buildhat/motor.html#buildhat.Motor.get
-# some more description of the functions can be found in the buildhat/motors.py file
-
 # Encoder_Example.py
 
 # Created by Noah Grzegorek on behalf of the ENGR 16X Teaching Team
