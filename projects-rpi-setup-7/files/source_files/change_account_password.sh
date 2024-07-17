b790cc4511bc07797f2e75038a493d85517d8bf0
Owen Bishop <owenlbishop@gmail.com>
2024-07-17 12:20:58 -0400

    Changed Change Password

--- projects-rpi-setup-7/files/source_files/change_account_password.sh
+++ projects-rpi-setup-7/files/source_files/change_account_password.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
+INFOPATH="/home/pi/info.sh"
+source $INFOPATH
+do
+    echo "Enter the new account password:"
+    read pswd1
+    echo "Enter the same password again"
+    read pswd2
+done while [ "$pswd1" != "$pswd2" ]
 
-passwd
-
-echo "Press [Enter] to continue."
-read
+for account in ${TEAMNUMS[@]}
+do
+    echo "Changing $account account password"
+    echo "$account:$pswd1" | chpasswd
+done
